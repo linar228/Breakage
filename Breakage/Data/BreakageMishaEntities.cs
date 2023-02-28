dbbf68a7c3e33e9d3f09e1a7321c933d4ce27984
линар валиев <linar369@mail.ru>
2023-03-01 01:36:47 +0300

    Добавьте файлы проекта.

---
+++ Breakage/Data/BreakageMishaEntities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakage.Data
+{
+    public partial class BreakageMishaEntities
+    {
+        private static BreakageMishaEntities _context;
+        public static BreakageMishaEntities GetContext()
+        {
+            if( _context == null )
+                _context = new BreakageMishaEntities();
+
+            return _context;
+        }
+    }
+}
---
+++ Breakage/Data/DataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakage.Data
+{
+    public class DataAccess
+    {
+        public delegate void RefreshListDelegate();
+        public static event RefreshListDelegate RefreshList;
+
+        public static List<Client> GetClients()
+        {
+            return BreakageMishaEntities.GetContext().Clients.ToList();
+        }
+
+        public static List<Service> GetServices()
+        {
+            return BreakageMishaEntities.GetContext().Services.ToList();
+        }
+
+        public static List<Gender> GetGenders()
+        {
+            return BreakageMishaEntities.GetContext().Genders.ToList();
+        }
+
+        public static void SaveClient(Client client)
+        {
+            if (client.ID == 0)
+                BreakageMishaEntities.GetContext().Clients.Add(client);
+
+            BreakageMishaEntities.GetContext().SaveChanges();
+            RefreshList?.Invoke();
+        }
+
+        public static void DeleteClient(Client client)
+        {
+            BreakageMishaEntities.GetContext().Clients.Remove(client);
+            BreakageMishaEntities.GetContext().SaveChanges();
+            RefreshList?.Invoke();
+        }
+    }
+}
